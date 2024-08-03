eb6506ef6a1029bba4c60b4052660de4742242d7
mrkoc <mr.kocta@bk.ru>
2024-08-04 00:37:11 +0300

    create annotations also fix bug with __tablename__

--- app/db/base.py
+++ app/db/base.py
@@ -10,8 +10,8 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
-    def __tablename__(cls):
-        return f"{cls.__class__.__name__.lower()}s"
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     typing_annotation_map = {str_200: String(200)}
