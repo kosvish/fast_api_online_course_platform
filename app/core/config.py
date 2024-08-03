e3462c2dc894e5d64291b891e829286f7592d18b
mrkoc <mr.kocta@bk.ru>
2024-08-04 00:02:48 +0300

    create class DbSettings

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,18 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
+from pydantic import BaseModel
 
 BASE_DIR_PATH = Path(__file__).parent.parent
+DB_PATH = BASE_DIR_PATH / "db.sqlite3"
+
+
+class DbSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = True
 
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR_PATH}/db.sqlite3"
-    db_echo: bool = True
+    db: DbSettings = DbSettings()
 
 
 settings = Settings()
