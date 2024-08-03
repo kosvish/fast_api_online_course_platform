fbdb7e0f1641bcd4b67615a2b58f2449a2b5f9ce
mrkoc <mr.kocta@bk.ru>
2024-08-04 00:02:20 +0300

    add declared attr in Base table

--- app/db/base.py
+++ app/db/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from typing import Annotated
 from sqlalchemy import String
+from sqlalchemy.orm import declared_attr
 
 str_200 = Annotated[str, 200]
 
@@ -8,7 +9,9 @@ str_200 = Annotated[str, 200]
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    @declared_attr.directive
+    def __tablename__(cls):
+        return f"{cls.__class__.__name__.lower()}s"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    typing_annotation_map = {
-        str_200: String(200)
-    }
+    typing_annotation_map = {str_200: String(200)}
