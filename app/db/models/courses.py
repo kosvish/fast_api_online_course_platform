0433588bd50ebde5bf61c52aa3b0182ccef52138
mrkoc <mr.kocta@bk.ru>
2024-08-09 00:43:35 +0300

    create relationships between course, users. By two methods, through secondary_table and association table

--- app/db/models/courses.py
+++ app/db/models/courses.py
@@ -1,11 +1,23 @@
-from ..base import Base, str_200
-from sqlalchemy.orm import Mapped
-from .mixins import UserRelationMixin
+from app.db.models.base import Base, str_200
+from sqlalchemy.orm import Mapped, relationship
+from .mixins import RelationMixin
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .course_user_association_table import CourseUserAssociation
+    from .users import User
 
 
-class Course(Base, UserRelationMixin):
+class Course(Base, RelationMixin):
     _user_back_populates = "courses"
     _user_unique = True
     title: Mapped[str_200]
     description: Mapped[str_200 | None] = None
     code_language: Mapped[str_200]
+    # participants_course
+    users: Mapped[list["User"]] = relationship(
+        back_populates="courses", secondary="course_user_association"
+    )
+    users_detail: Mapped[list["CourseUserAssociation"]] = relationship(
+        back_populates="course"
+    )
--- app/db/models/users.py
+++ app/db/models/users.py
@@ -1,8 +1,19 @@
-from ..base import Base, str_200
-from sqlalchemy.orm import Mapped, mapped_column
+from app.db.models.base import Base, str_200
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .courses import Course
+    from .course_user_association_table import CourseUserAssociation
 
 
 class User(Base):
     username: Mapped[str_200]
     email: Mapped[str_200] = mapped_column(unique=True, nullable=False)
     hash_password: Mapped[str_200] = mapped_column(unique=False, nullable=False)
+    course_details: Mapped[list["CourseUserAssociation"]] = relationship(
+        back_populates="user"
+    )
+    courses: Mapped[list["Course"]] = relationship(
+        back_populates="users", secondary="course_user_association"
+    )
