dbaa77bea9362a8752de19a55aaeabfcbcb387ab
mrkoc <mr.kocta@bk.ru>
2024-08-10 15:14:08 +0300

    create test url to database

--- app/core/config.py
+++ app/core/config.py
@@ -4,10 +4,12 @@ from pydantic import BaseModel
 
 BASE_DIR_PATH = Path(__file__).parent.parent
 DB_PATH = BASE_DIR_PATH / "db.sqlite3"
+DB_TEST_PATH = BASE_DIR_PATH / "db_test.sqlite3"
 
 
 class DbSettings(BaseModel):
     url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    test_url: str = f"sqlite+aiosqlite:///{DB_TEST_PATH}"
     echo: bool = True
 
 
