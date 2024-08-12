7939f96e1c7bb32de5f64934429ebbb68da321ed
mrkoc <mr.kocta@bk.ru>
2024-08-12 23:07:02 +0300

    remove extra field, isolating tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,61 +4,51 @@ from app.db import async_session_test, async_engine_test, async_engine
 from app.db.models import Base, UserModel, CourseModel
 from app.crud import (
     create_user,
-    read_user_by_id,
+    select_course_by_id,
     update_user_by_id,
     delete_user_by_id,
     count_all_users,
-    delete_all_users,
+    delete_all_users, select_user_by_id,
 )
 
 import pytest
 
 
 # username: str, email: str, hash_password: str
-@pytest.fixture
-def users():
-    users_data = [
-        ["Test1", "test1@gmail.com", "123"],
-        ["Test2", "test2@gmail.com", "123"],
-        ["Test3", "test3@gmail.com", "123"],
-    ]
-    return users_data
 
 
 @pytest.fixture(scope="function")
-async def clear_users(session):
-    await delete_all_users(session)
+async def setup_users(session, users_data):
+    for user in users_data:
+        await create_user(session, **user)
+
     yield
+    await delete_all_users(session)
 
 
-@pytest.mark.usefixtures("clear_users")
+@pytest.mark.usefixtures("setup_users")
 class TestCrudUser:
-    async def test_create_user(self, session: AsyncSession, users):
-        for user in users:
-            await create_user(session, user[0], user[1], user[2])
 
-        count_users = await count_all_users(session)
-        assert count_users == len(users)
+    async def test_select_user_by_id(self, session: AsyncSession, users_data):
 
-    async def test_select_user_by_id(self, session: AsyncSession, users):
-        user = await create_user(session, *users[0])
+        current_user = await select_user_by_id(session, 1)
+        assert current_user.username == users_data[0]["username"]
 
-        current_user = await read_user_by_id(session, 1)
-        assert current_user.username == user.username
-
-    async def test_update_user_by_id(self, session: AsyncSession, users):
-        user = await create_user(session, *users[0])
+    async def test_update_user_by_id(self, session: AsyncSession, users_data):
         updated_user = await update_user_by_id(
             session, 1, "Test1Update", "test1update@gmail.com", "1234"
         )
-        refreshed_user = await read_user_by_id(session, 1)
+        refreshed_user = await select_user_by_id(session, 1)
 
         assert updated_user.username == refreshed_user.username
         assert updated_user.email == refreshed_user.email
         assert updated_user.hash_password == refreshed_user.hash_password
 
-    async def test_delete_user_by_id(self, session: AsyncSession, users):
-        user = await create_user(session, *users[0])
+    async def test_delete_user_by_id(self, session: AsyncSession, users_data):
         await delete_user_by_id(session, 1)
         count_users = await count_all_users(session)
-        assert count_users == 0
+        assert count_users == len(users_data) - 1
+
+    async def test_count_all_users(self, session, users_data):
+        result = await count_all_users(session)
+        assert result == len(users_data)
