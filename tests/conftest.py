c01c4498b6c71ffbb8a0f2fc2769443805e335fd
mrkoc <mr.kocta@bk.ru>
2024-08-12 23:04:04 +0300

    add main courses, users info

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,38 @@ async def setup_database():
 async def session():
     async with async_session_test() as s:
         yield s
+
+
+@pytest.fixture
+def users_data():
+    users_data = [
+        {"username": "Test1", "email": "test1@gmail.com", "hash_password": "123"},
+        {"username": "Test2", "email": "test2@gmail.com", "hash_password": "123"},
+        {"username": "Test3", "email": "test3@gmail.com", "hash_password": "123"},
+    ]
+    return users_data
+
+
+@pytest.fixture
+def courses_data():
+    courses_data = [
+        {
+            "title": "Django",
+            "description": "Best Course for Django",
+            "code_language": "Python",
+            "user_id": 1,
+        },
+        {
+            "title": "FastAPI",
+            "description": "Best Course for FastAPI",
+            "code_language": "Python",
+            "user_id": 2,
+        },
+        {
+            "title": "SQLAlchemy",
+            "description": "Best Course for SQLAlchemy",
+            "code_language": "Python",
+            "user_id": 3,
+        },
+    ]
+    return courses_data
