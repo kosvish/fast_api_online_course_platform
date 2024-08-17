9b943755907b776ea616443b7de951803ccc97e3
mrkoc <mr.kocta@bk.ru>
2024-08-17 23:57:43 +0300

    delete extra func, create func for select course with participants, with creator,

--- app/crud/course_user_relationship.py
+++ app/crud/course_user_relationship.py
@@ -1,27 +1,58 @@
+from fastapi import HTTPException
+from fastapi import status
 from sqlalchemy import select, delete
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models import UserModel, CourseModel, CourseUserAssociation
 from .course import select_course_by_id
 from .user import select_user_by_id
+from app.utils import check_user_in_course_participants
 
 
-async def select_all_courses_with_users_in_it(
+async def select_all_courses_with_participants(
     session: AsyncSession,
 ) -> list[CourseModel]:
-    query = select(CourseModel).options(
-        selectinload(CourseModel.users_detail).joinedload(CourseUserAssociation.user)
+    query = (
+        select(CourseModel)
+        .options(selectinload(CourseModel.participants))
+        .order_by(CourseModel.id)
     )
-    result = await session.scalars(query)
-    return list(result)
+
+    courses = await session.scalars(query)
+
+    return list(courses)
 
 
-async def add_user_in_course(session: AsyncSession, user_id: int, course_id: int):
-    course = await select_course_by_id(session, course_id)
-    user = await select_user_by_id(session, user_id)
-    course.users_detail.append(CourseUserAssociation(
-        user=user
-    ))
+async def select_course_with_participants_by_id(
+    session: AsyncSession, course_id: int
+) -> CourseModel | None:
+    query = (
+        select(CourseModel)
+        .where(CourseModel.id == course_id)
+        .options(selectinload(CourseModel.participants))
+        .order_by(CourseModel.id)
+    )
+
+    course = await session.scalar(query)
+    return course
+
+
+async def select_course_with_creator_by_id(
+    session: AsyncSession, course_id: int
+) -> CourseModel | None:
+    query = (
+        select(CourseModel)
+        .where(CourseModel.id == course_id)
+        .options(joinedload(CourseModel.creator))
+    )
+    course = await session.scalar(query)
+    return course
+
+
+async def add_participants_to_course_by_id(
+    session: AsyncSession, course: CourseModel, *participants: UserModel
+):
+    for user in participants:
+        course.participants.append(user)
 
     await session.commit()
-
