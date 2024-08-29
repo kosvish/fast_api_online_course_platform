eb3ae8c60309e0fc43b630650b6accd3bf45ecc0
mrkoc <mr.kocta@bk.ru>
2024-08-29 19:20:29 +0300

    add imports to __init__.py

--- app/api/schemas/__init__.py
+++ app/api/schemas/__init__.py
@@ -1,2 +1,2 @@
 from .courses import Course, CourseUpdate, CourseUpdatePartial, CourseCreate
-from .users import CreateUser, UpdateUser, UpdateUserPartial, ResponseUser
+from .users import CreateUser, UpdateUser, UpdateUserPartial, ResponseUser, UserSchema
