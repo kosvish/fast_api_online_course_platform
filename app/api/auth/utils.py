93c87402c9a131fcd88c91ac92dd97f1cf9e575e
mrkoc <mr.kocta@bk.ru>
2024-08-30 18:10:05 +0300

    create utils func for encode, decode token, hash and validate password

---
+++ app/api/auth/utils.py
@@ -0,0 +1,42 @@
+from datetime import timedelta, datetime
+import jwt
+import bcrypt
+from app.core import settings
+
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    hashed_pwd: bytes = password.encode()
+    return bcrypt.hashpw(hashed_pwd, salt)
+
+
+def validate_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
+
+def encode_jwt_token(
+    payload: dict,
+    private_key: str = settings.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta = timedelta(minutes=15),
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.utcnow()
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(exp=expire, iat=now)
+    encoded = jwt.encode(payload, private_key, algorithm=algorithm)
+    return encoded
+
+
+def decode_jwt_token(
+    token: str | bytes,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
+    data = jwt.decode(token, public_key, algorithms=[algorithm])
+    return data
