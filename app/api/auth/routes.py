f3d236f94c7b8bdd9562dd8b393247669829aa10
mrkoc <mr.kocta@bk.ru>
2024-08-30 18:08:42 +0300

    create auth jwt route for logging user

---
+++ app/api/auth/routes.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, Form, HTTPException, status, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .utils import validate_password, encode_jwt_token
+from fastapi.security import OAuth2PasswordBearer
+from app.db.models import UserModel
+from ..dependencies import get_async_session
+from .schemas import TokenInfo
+
+
+router = APIRouter(prefix="/jwt", tags=["AuthJWT"])
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/jwt/login")
+
+
+async def validate_auth_user(
+    email: str = Form(),
+    password: str = Form(),
+    session: AsyncSession = Depends(get_async_session),
+):
+    invalid_data_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password"
+    )
+    query = select(UserModel).where(UserModel.email == email)
+    user = await session.scalar(query)
+    if user is None:
+        raise invalid_data_exc
+
+    if not validate_password(password, user.hash_password):
+        raise invalid_data_exc
+    return user
+
+
+@router.post("/login")
+async def login_user(user: UserModel = Depends(validate_auth_user)):
+    jwt_payload = {"sub": user.id, "username": user.username, "email": user.email}
+    token = encode_jwt_token(jwt_payload)
+    return TokenInfo(token=token, type="Bearer")
