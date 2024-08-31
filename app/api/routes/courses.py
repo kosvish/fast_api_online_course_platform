f7bafe3e035df266886eccf8d75b057a2a960b33
mrkoc <mr.kocta@bk.ru>
2024-08-31 22:43:56 +0300

    delete old logics, create CRUD routes to course, also add course enroll route

--- app/api/routes/courses.py
+++ app/api/routes/courses.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.models import CourseModel
-from app.api.dependencies import get_async_session, get_course_by_id
-from app.api.schemas import (
-    CourseCreate,
-    Course,
-    CourseUpdate,
-    CourseUpdatePartial,
-)
+
+from app.api import CourseUpdatePartial
+from app.api.dependencies import get_async_session
+from app.api.schemas import Course, CourseResponse, CourseCreateProd
 from fastapi import status
-from app.crud.course import select_all_courses, create_course
+from app.crud.course import (
+    select_all_courses,
+    create_course,
+    update_course_by_id,
+    select_course_by_id,
+    delete_course_by_id,
+)
+from app.db.models import UserModel
+from app.api.dependencies import get_current_user_by_token, get_course_by_id
+from app.crud.course_user_relationship import select_course_with_participants_by_id
+from app.db.models import CourseModel
+from app.utils import check_is_owner_user_course, check_user_in_course_participants
 
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
@@ -23,82 +29,97 @@ async def get_all_courses(
     return courses
 
 
-@router.post("/create", status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/create", status_code=status.HTTP_201_CREATED, response_model=CourseResponse
+)
 async def create_courses(
-    course: CourseCreate, session: AsyncSession = Depends(get_async_session)
+    course_create: CourseCreateProd,
+    session: AsyncSession = Depends(get_async_session),
+    current_user: UserModel = Depends(get_current_user_by_token),
+):
+    course = await create_course(
+        session,
+        title=course_create.title,
+        description=course_create.description,
+        code_language=course_create.code_language,
+        creator_id=current_user.id,
+    )
+    await session.refresh(course)
+    return CourseResponse(
+        title=course.title,
+        description=course.description,
+        code_language=course.code_language,
+    )
+
+
+@router.get("/{course_id}/enroll", status_code=status.HTTP_200_OK)
+async def enroll_course(
+    course_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    current_user: UserModel = Depends(get_current_user_by_token),
 ) -> dict:
-    new_course = await create_course(session, )
+
+    course = await select_course_with_participants_by_id(session, course_id)
+    if check_user_in_course_participants(course, current_user):
+        course.participants.append(current_user)
+        await session.commit()
+        await session.refresh(course)
+        return {
+            "status": status.HTTP_200_OK,
+            "message": f"You successfully joined at course {course.title}!",
+        }
 
 
-#
-#
-# @router.get("/{course_id}", status_code=status.HTTP_200_OK)
-# async def get_course(
-#     course: CourseModel = Depends(get_course_by_id),
-# ):
-#     return course
-#
-#
-# @router.put("/update/{course_id}", status_code=status.HTTP_200_OK)
-# async def update_course(
-#     course_scheme: CourseUpdate,
-#     session: AsyncSession = Depends(get_async_session),
-#     course: CourseModel = Depends(get_course_by_id),
-# ) -> dict:
-#
-#     for name, value in course_scheme.model_dump().items():
-#         setattr(course, name, value)
-#     session.add(course)
-#     await session.commit()
-#
-#     await session.refresh(course)
-#
-#     return {
-#         "message": "Successfully updated course",
-#         "course": {
-#             "title": course.title,
-#             "description": course.description,
-#             "code_language": course.code_language,
-#             "mentor": course.mentor,
-#         },
-#         "status": status.HTTP_200_OK,
-#     }
-#
-#
-# @router.patch("/update/{course_id}", status_code=status.HTTP_200_OK)
-# async def update_course_partial(
-#     course_scheme: CourseUpdatePartial,
-#     session: AsyncSession = Depends(get_async_session),
-#     course: CourseModel = Depends(get_course_by_id),
-# ) -> dict:
-#
-#     for name, value in course_scheme.model_dump(exclude_unset=True).items():
-#         setattr(course, name, value)
-#     session.add(course)
-#     await session.commit()
-#
-#     await session.refresh(course)
-#
-#     return {
-#         "message": "Successfully updated course",
-#         "course": {
-#             "title": course.title,
-#             "description": course.description,
-#             "code_language": course.code_language,
-#             "mentor": course.mentor,
-#         },
-#         "status": status.HTTP_200_OK,
-#     }
-#
-#
-# @router.delete("/delete/{course_id}", status_code=status.HTTP_200_OK)
-# async def delete_course(
-#     session: AsyncSession = Depends(get_async_session),
-#     course: CourseModel = Depends(get_course_by_id),
-# ) -> dict:
-#     await session.delete(course)
-#     await session.commit()
-#     return {
-#         "message": "Successfully deleted course",
-#         "status": status.HTTP_200_OK,
-#     }
+@router.put(
+    "/{course_id}/update", response_model=CourseResponse, status_code=status.HTTP_200_OK
+)
+async def update_course_route(
+    course_id: int,
+    update_course: CourseUpdatePartial,
+    session: AsyncSession = Depends(get_async_session),
+    current_user: UserModel = Depends(get_current_user_by_token),
+):
+    try:
+        course = await select_course_by_id(session, course_id)
+        if check_is_owner_user_course(course, current_user):
+            course = await update_course_by_id(
+                session, course_id, **update_course.model_dump(exclude_none=True)
+            )
+            return CourseResponse(
+                title=course.title,
+                description=course.description,
+                code_language=course.code_language,
+            )
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Course with id: {course_id} not found",
+        )
+
+
+@router.get(
+    "/{course_id}", response_model=CourseResponse, status_code=status.HTTP_200_OK
+)
+async def get_course_by_id_route(
+    course: CourseModel = Depends(get_course_by_id),
+):
+    return CourseResponse(
+        title=course.title,
+        description=course.description,
+        code_language=course.code_language,
+    )
+
+
+@router.delete("/{course_id}/delete", status_code=status.HTTP_200_OK)
+async def delete_course_by_id_route(
+    course_id: int,
+    current_user: UserModel = Depends(get_current_user_by_token),
+    session: AsyncSession = Depends(get_async_session),
+):
+    course = await select_course_by_id(session, course_id)
+    if check_is_owner_user_course(course, current_user):
+        await delete_course_by_id(session, course_id)
+    return {
+        "status": status.HTTP_200_OK,
+        "message": f"Course {course.title} was successfully deleted!",
+    }
