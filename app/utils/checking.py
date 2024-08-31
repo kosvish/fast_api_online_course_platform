ef5bb20b9cb69d4a9992a7cdbcbd668f28ec5258
mrkoc <mr.kocta@bk.ru>
2024-08-31 22:42:54 +0300

    create func to check user in course, and owns course by user

--- app/utils/checking.py
+++ app/utils/checking.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models import CourseModel, UserModel
 
@@ -5,7 +6,18 @@ from app.db.models import CourseModel, UserModel
 def check_user_in_course_participants(
     course_with_participants: CourseModel, user: UserModel
 ):
-    if user not in course_with_participants.participants:
-        return True
-    else:
-        return False
+    if user in course_with_participants.participants:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"You already joined at course: {course_with_participants.title}",
+        )
+    return True
+
+
+def check_is_owner_user_course(course: CourseModel, user: UserModel):
+    if course.creator_id != user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You cannot edit, this course'
+        )
+    return True
