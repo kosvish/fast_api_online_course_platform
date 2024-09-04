5ee077674419d44ce9a3a15f8fc5b1076797ff61
mrkoc <mr.kocta@bk.ru>
2024-09-04 16:15:44 +0300

    rework logic create router, change path to show pages

--- app/pages_routes/main.py
+++ app/pages_routes/main.py
@@ -5,13 +5,11 @@ import os
 
 
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 templates_dir = os.path.join(base_dir, "templates")
-
-router = APIRouter(prefix="/pages", tags=["Pages"])
+router = APIRouter()
 templates = Jinja2Templates(directory=templates_dir)
 
 
 @router.get("/main", status_code=status.HTTP_200_OK)
 async def get_main_page(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+    return templates.TemplateResponse("/main/main.html", {"request": request})
