6e431bdc42ba49e24d3c6489bc2add40ac2a5168
mrkoc <mr.kocta@bk.ru>
2024-09-04 16:15:15 +0300

    rework logic create router in init file, also add prefixes

--- app/pages_routes/__init__.py
+++ app/pages_routes/__init__.py
@@ -1,7 +1,9 @@
 from .main import router as main_router
+from .course import router as course_router
 from fastapi import APIRouter
 
 
-router = APIRouter()
+router = APIRouter(prefix='/pages', tags=['Pages'])
 
 router.include_router(main_router)
+router.include_router(course_router, prefix='/courses')
