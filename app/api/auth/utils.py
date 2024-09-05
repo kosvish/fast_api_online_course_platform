2fdec5216622ab36440ec02b745c3ad3afa74459
mrkoc <mr.kocta@bk.ru>
2024-09-05 14:23:04 +0300

    rework logic, remove tokens depenencies into utils of auth

--- app/api/auth/utils.py
+++ app/api/auth/utils.py
@@ -1,7 +1,25 @@
 from datetime import timedelta, datetime
 import jwt
 import bcrypt
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+
 from app.core import settings
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/jwt/login", scheme_name="UserSchema")
+
+
+async def get_current_token_payload(token: str = Depends(oauth2_scheme)) -> dict:
+    print(token)
+    try:
+        payload = decode_jwt_token(token)
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token: {e}"
+        )
+    return payload
 
 
 def hash_password(password: str) -> bytes:
@@ -20,7 +38,7 @@ def encode_jwt_token(
     algorithm: str = settings.auth_jwt.algorithm,
     expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
     expire_timedelta: timedelta = timedelta(minutes=15),
-)-> str:
+) -> str:
     to_encode = payload.copy()
     now = datetime.utcnow()
     if expire_timedelta:
