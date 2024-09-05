59078cd578693b7911445ec8e05df62ef364a4e9
mrkoc <mr.kocta@bk.ru>
2024-09-05 14:18:05 +0300

    create new pages router for view html detail of course

--- app/pages_routes/course.py
+++ app/pages_routes/course.py
@@ -1,7 +1,7 @@
 from .main import templates
-from fastapi import Request, Depends
+from fastapi import Request, Depends, status
 from app.db.models import CourseModel
-from ..api.routes.courses import get_all_courses
+from ..api.routes.courses import get_all_courses, get_course_by_id_route
 from fastapi import APIRouter
 
 
@@ -14,4 +14,15 @@ async def get_all_courses_route_page(
 ):
     return templates.TemplateResponse(
         "courses/main.html", context={"request": request, "courses": courses}
+    )
+
+
+@router.get("/{course_id}", status_code=status.HTTP_200_OK)
+async def get_courses_by_id(
+    request: Request,
+    course_id: int,
+    course: CourseModel = Depends(get_course_by_id_route),
+):
+    return templates.TemplateResponse(
+        "courses/current_course.html", context={"request": request, "course": course}
     )
