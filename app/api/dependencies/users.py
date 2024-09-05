010dd4db2f82995aa76215de9b516f7afa2e3852
mrkoc <mr.kocta@bk.ru>
2024-09-05 14:21:43 +0300

    while fix error with imports, rework logic to get_course not with func select_user_by_id

--- app/api/dependencies/users.py
+++ app/api/dependencies/users.py
@@ -1,17 +1,17 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.crud.user import select_user_by_id
-
 from app.db.models import UserModel
 from fastapi import HTTPException, status
 from fastapi import Depends
 from .db import get_async_session
-from ..auth.routes import get_current_token_payload
+from ..auth.utils import get_current_token_payload
 
 
 async def get_user_by_id(
     user_id: int, session: AsyncSession = Depends(get_async_session)
 ):
-    user = await select_user_by_id(session, user_id)
+    query = select(UserModel).where(UserModel.id == user_id).order_by(UserModel.id)
+    user = await session.scalar(query)
     if user is not None:
         return user
     else:
@@ -26,7 +26,8 @@ async def get_current_user_by_token(
     session: AsyncSession = Depends(get_async_session),
 ) -> UserModel:
     user_id = payload.get("sub")
-    user = await select_user_by_id(session, user_id)
+    query = select(UserModel).where(UserModel.id == user_id).order_by(UserModel.id)
+    user = await session.scalar(query)
     if user is not None:
         return user
     else:
