4ca42f527cd2a01d7c4f6a0a93cd57db4b720c86
mrkoc <mr.kocta@bk.ru>
2024-09-05 14:22:29 +0300

    change data type of hash_password from Text to bytes

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -1,6 +1,7 @@
 from app.db.models.base import Base, str_200
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
+from sqlalchemy import LargeBinary
 
 if TYPE_CHECKING:
     from .courses import Course
@@ -8,9 +9,10 @@ if TYPE_CHECKING:
 
 
 class User(Base):
+    __tablename__ = 'users'
     username: Mapped[str_200]
     email: Mapped[str_200] = mapped_column(unique=True, nullable=False)
-    hash_password: Mapped[str_200] = mapped_column(unique=False, nullable=False)
+    hash_password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False, unique=False)
     enrolled_course: Mapped[list["Course"]] = relationship(
         back_populates="participants", secondary="course_user_association"
     )
