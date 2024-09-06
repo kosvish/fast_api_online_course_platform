52d567ef2e1b3fce4cd2079f2a1d3a76c2ba145a
mrkoc <mr.kocta@bk.ru>
2024-09-06 15:38:21 +0300

    add a new users router to a main pages route

--- app/pages_routes/__init__.py
+++ app/pages_routes/__init__.py
@@ -1,5 +1,6 @@
 from .main import router as main_router
 from .course import router as course_router
+from .user import router as user_router
 from fastapi import APIRouter
 
 
@@ -7,3 +8,4 @@ router = APIRouter(prefix='/pages', tags=['Pages'])
 
 router.include_router(main_router)
 router.include_router(course_router, prefix='/courses')
+router.include_router(user_router, prefix='/users')
