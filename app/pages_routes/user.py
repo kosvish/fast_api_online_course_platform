bfe9c40df8f829ba3e530541ec2270d57aece94e
mrkoc <mr.kocta@bk.ru>
2024-09-06 15:40:15 +0300

    create a two user registration router, with form (GET) html and POST

---
+++ app/pages_routes/user.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Form
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .main import templates
+from fastapi import Request, Depends, status
+from typing import Annotated
+
+from ..api.dependencies import get_async_session
+from ..api.routes.users import create_user_route
+from ..api.schemas import CreateUser
+
+router = APIRouter()
+
+
+@router.get("/registration")
+async def user_registration_form_get(request: Request):
+    return templates.TemplateResponse("/users/registration.html", {"request": request})
+
+
+@router.post("/registration")
+async def user_registration_post(
+    request: Request,
+    username: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    session: AsyncSession = Depends(get_async_session)
+):
+    data_dict = CreateUser(username=username, email=email, hash_password=password)
+    response = await create_user_route(user_data=data_dict, session=session)
+    if response:
+        return templates.TemplateResponse("/main/main.html", {"request": request})
