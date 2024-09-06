23be802045aa548358634fa7d4e94b4eac542ea7
mrkoc <mr.kocta@bk.ru>
2024-09-06 17:02:02 +0300

    add new routes pages for login user

--- app/pages_routes/user.py
+++ app/pages_routes/user.py
@@ -5,6 +5,7 @@ from .main import templates
 from fastapi import Request, Depends, status
 from typing import Annotated
 
+from ..api.auth.routes import validate_auth_user
 from ..api.dependencies import get_async_session
 from ..api.routes.users import create_user_route
 from ..api.schemas import CreateUser
@@ -23,9 +24,26 @@ async def user_registration_post(
     username: Annotated[str, Form()],
     email: Annotated[str, Form()],
     password: Annotated[str, Form()],
-    session: AsyncSession = Depends(get_async_session)
+    session: AsyncSession = Depends(get_async_session),
 ):
     data_dict = CreateUser(username=username, email=email, hash_password=password)
     response = await create_user_route(user_data=data_dict, session=session)
     if response:
         return templates.TemplateResponse("/main/main.html", {"request": request})
+
+
+@router.post("/login")
+async def user_login_post(
+    request: Request,
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    session: AsyncSession = Depends(get_async_session),
+):
+    data = await validate_auth_user(username, password, session)
+    if data:
+        return templates.TemplateResponse("/main/main.html", {"request": request})
+
+
+@router.get("/login")
+async def user_login_get(request: Request):
+    return templates.TemplateResponse("/users/login.html", {"request": request})
