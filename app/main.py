68f9f26b119a67790188a5a1dec1168146af1dd5
mrkoc <mr.kocta@bk.ru>
2024-09-09 14:45:00 +0300

    create middleware class for get token from cookie and insert into header authorization

--- app/main.py
+++ app/main.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
+
 from app.db import session
 from app.db.models import base
 from contextlib import asynccontextmanager
 import uvicorn
 from app.api.routes import course_router, user_router
 from app.api.demo_auth.views import router as demo_auth_router
+from fastapi import Request
+
 
 # from app.api.demo_auth.demo_jwt_auth import router as demo_auth_jwt_router
 from app.api.auth import auth_router
 from fastapi.staticfiles import StaticFiles
 from app.pages_routes import router as pages_router
 import os
+
+
+class AddAuthHeaderMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        token = request.cookies.get("access_token")
+        if token:
+            # Добавляем токен в заголовок Authorization, если он есть
+            request.headers.__dict__["_list"].append(
+                (b"authorization", f"Bearer {token}".encode())
+            )
+        response = await call_next(request)
+        return response
 
 
 @asynccontextmanager
@@ -27,6 +43,7 @@ app.include_router(user_router)
 # app.include_router(demo_auth_jwt_router)
 app.include_router(auth_router)
 app.include_router(pages_router)
+app.add_middleware(AddAuthHeaderMiddleware)
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 static_dir = os.path.join(base_dir, "static")
