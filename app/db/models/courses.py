3e31ca3bcc3f9eaaefa8086065b948fab9ea06b3
mrkoc <mr.kocta@bk.ru>
2024-09-19 21:21:07 +0300

    update model Course adding image field, and adding descr field can be null with price

--- app/db/models/courses.py
+++ app/db/models/courses.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import ForeignKey
 
 from app.db.models.base import Base, str_200
@@ -9,14 +11,24 @@ if TYPE_CHECKING:
     from .course_user_association_table import CourseUserAssociation
     from .users import User
 
+base_dir = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+images_dir = os.path.join(base_dir, "course_images")
+
 
 class Course(Base):
     title: Mapped[str_200]
-    description: Mapped[str_200 | None] = None
-    code_language: Mapped[str_200]
-    price: Mapped[int] = mapped_column(default=500, server_default="500")
-    creator_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
+    description: Mapped[str | None]
+    code_language: Mapped[str]
+    price: Mapped[int | None] = mapped_column(default=0, server_default="0")
+    creator_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
     participants: Mapped[list["User"]] = relationship(
         back_populates="enrolled_course", secondary="course_user_association"
     )
-    creator: Mapped['User'] = relationship(back_populates='created_courses')
+    image_path: Mapped[str] = mapped_column(
+        nullable=True,
+        default=f"{images_dir}/courses-1.jpg",
+        server_default='f"{images_dir}/courses-1.jpg"',
+    )
+    creator: Mapped["User"] = relationship(back_populates="created_courses")
