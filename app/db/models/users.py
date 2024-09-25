13495b683974381b35e92837b12f198d58541680
mrkoc <mr.kocta@bk.ru>
2024-09-25 17:18:59 +0300

    adding new column image_path for user, also create relation course_user_rating_association

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -5,15 +5,26 @@ from sqlalchemy import LargeBinary
 
 if TYPE_CHECKING:
     from .courses import Course
-    from .course_user_association_table import CourseUserAssociation
+    from .course_rating_association import CourseRatingAssociation
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     username: Mapped[str_200]
     email: Mapped[str_200] = mapped_column(unique=True, nullable=False)
-    hash_password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False, unique=False)
+    hash_password: Mapped[bytes] = mapped_column(
+        LargeBinary, nullable=False, unique=False
+    )
     enrolled_course: Mapped[list["Course"]] = relationship(
         back_populates="participants", secondary="course_user_association"
     )
-    created_courses: Mapped[list['Course']] = relationship(back_populates='creator')
+    created_courses: Mapped[list["Course"]] = relationship(back_populates="creator")
+    image_path: Mapped[str] = mapped_column(
+        nullable=True,
+        default="/user_images/default.jpg",
+        server_default="/user_images/default.jpg",
+    )
+
+    rated_courses: Mapped[list["CourseRatingAssociation"]] = relationship(
+        back_populates="user", cascade="all, delete"
+    )
