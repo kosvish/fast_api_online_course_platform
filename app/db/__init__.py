07ff6cebf25d59d525db4494661218a9c53f514c
mrkoc <mr.kocta@bk.ru>
2024-09-25 17:13:24 +0300

    add course_ratings association to init file

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,3 +1,3 @@
-from .models import CourseModel, UserModel, ProfileModel #CourseUserAssociation
+from .models import CourseModel, UserModel, ProfileModel, CourseRatingAssociation
 from .session import async_session, async_session_test, async_engine_test, async_engine
 
--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -2,6 +2,7 @@ from .courses import Course as CourseModel
 from .users import User as UserModel
 from .profiles import Profile as ProfileModel
 from .mixins import RelationMixin
+from .course_rating_association import CourseRatingAssociation
 from .course_user_association_table import CourseUserAssociation
 from .base import Base
 
@@ -11,5 +12,6 @@ __all__ = [
     'ProfileModel',
     'RelationMixin',
     'CourseUserAssociation',
-    'Base'
+    'Base',
+    'CourseRatingAssociation'
 ]
